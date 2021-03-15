24263b80a52606264f8ea390296d43b04f1f2326
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-03-15 17:59:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,29 @@ var nav_outer = document.querySelector('nav');
 
 
 
+// object.addEventListener("scroll", scrollfunction());
+
+//  var scrollfunction =  () => {
+//    debugger
+//     if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+//        console.log('done')
+//       nav_outer.style.backgroundColor = "white";
+      
+//     } else {
+//       nav_outer.style.backgroundColor = "transparent";
+      
+//     }
+// }
 
 
-window.addEventListener( "scroll", () => {
-    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
-        alert('vijul');
-      nav_outer.style.backgroundColor = "white";
-      
-    } else {
-      nav_outer.style.backgroundColor = "transparent";
-      
-    }
-})
+var typed = new Typed('.type', {
+  strings: ["UI/UX Developer", "Web and Graphic Designer", "React JS Developer"],
+  showCursor: false,
+  loop:true,
+  loopCount: Infinity, 
+  
+  typeSpeed: 100
+});
 
 menu.addEventListener("click",() => {
     nav.style.width = "50vw";
@@ -24,6 +35,10 @@ menu.addEventListener("click",() => {
 cancel.addEventListener("click", () => {
      nav.style.width = "0vw";
  })
+
+
+
+
 
 
 
